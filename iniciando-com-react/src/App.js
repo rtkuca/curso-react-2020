ec357ee634e9960ce97dc3f1a3ef7f93e4aab019
rtkuca <rtkuca@hotmail.com>
2021-07-10 10:00:09 -0300

    criacao do projeto cadastro-produtos

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -1,25 +1,97 @@
 import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+class App extends React.Component
+{
+    state = {
+        nome : ''
+    }
+
+    // fazer bind da funcao
+    // constructor()
+    // {
+    //     super()
+    //     this.modificarNome = this.modificarNome.bind(this)
+    // }
+
+    // modificarNome(event){
+    //     this.setState({
+    //         nome: event.target.value
+    //     })
+    // }
+
+
+    modificarNome = (event) => {
+        this.setState({
+            nome: event.target.value
+        })
+    }
+
+    criaComboBox = () =>
+    {
+        const opcoes = ["Fulano", "Ciclano"];
+        const comboBoxOpcoes = opcoes.map( opcao => <option key={opcao}>{opcao}</option> )
+
+        return(
+            <select>
+                {comboBoxOpcoes}
+            </select>
+        )
+    }
+
+    componentDidMount() {
+        console.log("Executou o componentDidMount")
+    }
+
+    render() {
+        console.log("Executou o render")
+        const MeuComboBox = () => this.criaComboBox()
+
+        return(
+            <>
+                <input type="text" value={this.state.nome} onChange={this.modificarNome}/>
+                <h1>Hello {this.state.nome}, NomeProps { this.props.nomeProps } </h1>
+                <MeuComboBox/>
+            </>
+        )
+    }
+}
+*/
+
+function App(props)
+{
+
+    const modificarNome = (event) =>
+    {
+        console.log(event.target.value)
+    }
+
+    const criaComboBox = () =>
+    {
+        const opcoes = ["Fulano", "Ciclano"];
+        const comboBoxOpcoes = opcoes.map( opcao => <option key={opcao}>{opcao}</option> )
+
+        return(
+            <select>
+                {comboBoxOpcoes}
+            </select>
+        )
+    }
+
+    console.log("Executou o render")
+    const MeuComboBox = () => criaComboBox()
+
+    return(
+        <>
+            <input type="text" className="texto-centralizado" value={props.nomeProps} onChange={modificarNome}/>
+            <h1>Hello  { props.nomeProps } </h1>
+            <MeuComboBox/>
+        </>
+    )
+
 }
 
+
 export default App;
+
